c706eab9a93f3f0fc25c705213592865cf770c00
Shady Khattab <shady@myparcel.com>
2019-04-02 13:57:35 +0200

    ✨ Implemented Model methods

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"errors"
 	"github.com/satori/go.uuid"
 	"time"
 )
@@ -17,15 +18,37 @@ type Model struct {
 // Method required to implement `jsonapi.UnmarshalIdentifier`. Converts the string representation of a uuid to the
 // internal Model representation.
 func (m *Model) SetID(id string) error {
-	panic("implement me")
+	if id == "" {
+		return nil
+	}
+
+	ID, err := uuid.FromString(id)
+	if err != nil {
+		return err
+	}
+
+	m.ID = ID
+
+	return nil
 }
 
 // Method required to implement `jsonapi.MarshalIdentifier`. Returns a string representation of the uuid of the Model.
 func (m *Model) GetID() string {
-	panic("implement me")
+	// An empty uuid yields a string representation of only zeroes.
+	if id := m.ID.String(); id != "00000000-0000-0000-0000-000000000000" {
+		return id;
+	}
+
+	return ""
 }
 
 // Callback method for `gorm` package. Makes sure a uuid is set before a Model is created.
 func (m *Model) BeforeCreate() error {
-	panic("implement me")
+	if m.DeletedAt != nil {
+		return errors.New("cannot create deleted Model")
+	}
+	if m.GetID() == "" {
+		m.ID = uuid.NewV4()
+	}
+	return nil
 }
